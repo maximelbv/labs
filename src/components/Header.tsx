d989db17df2346492271f26f1c33faee9702866d
Maxime Lefebvre <lefebvremaxime00@gmail.com>
2025-03-23 17:49:31 +0100

    modify header & remove footer

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,18 @@
 import { Link } from "react-router";
 import Logo from "./Logo";
+import { MAXIMELBV_LINK } from "../constants/links";
 
 const Header = () => {
   return (
     <div className="flex justify-start p-4">
-      <Link to="/">
-        <Logo />
-      </Link>
+      <div className="flex flex-col gap-1.5">
+        <a href={MAXIMELBV_LINK} target="blank_">
+          <img className="!object-contain" src="/maximelbv-logo.svg" />
+        </a>
+        <Link to="/">
+          <Logo />
+        </Link>
+      </div>
     </div>
   );
 };
--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,13 +1,11 @@
 import { ReactNode } from "react";
 import Header from "../components/Header";
-import Footer from "../components/Footer";
 
 const MainLayout = ({ children }: { children: ReactNode }) => {
   return (
     <div>
       <Header />
       <div className="p-4">{children}</div>
-      <Footer />
     </div>
   );
 };

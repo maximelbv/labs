227a3c0ec7a2689d06173a41868f12e8e99d1dc8
Maxime Lefebvre <lefebvremaxime00@gmail.com>
2025-05-22 00:20:19 +0200

    improve preview card

--- src/components/ExperimentCard.tsx
+++ src/components/ExperimentCard.tsx
@@ -16,13 +16,17 @@ const ExperimentCard = ({
   return (
     <Link key={title} to={`/${slug}`}>
       <div className="group relative flex items-center border border-border-light justify-center rounded-xl bg-white aspect-video overflow-hidden">
-        <img src={cover} />
-      </div>
-      <div className="flex flex-col">
-        <span className="text-text-muted">{category}</span>
-        <span className="font-bold text-2xl text-text-primary leading-6">
-          {title}
-        </span>
+        <img
+          src={cover}
+          className="w-full h-full object-cover transition-opacity"
+        />
+        <div className="absolute inset-0 bg-white opacity-0 group-hover:opacity-50 transition-opacity pointer-events-none" />
+        <div className="group-hover:flex hidden absolute flex-col items-center z-10">
+          <span className="text-text-muted">{category}</span>
+          <span className="font-bold text-2xl text-text-primary leading-6">
+            {title}
+          </span>
+        </div>
       </div>
     </Link>
   );
